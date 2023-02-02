07a0f0315d5f2c84431572890f6079b8af606221
mohadeev <94206815+mohadeev@users.noreply.github.com>
2023-02-02 22:19:53 +0100

    DF

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0032b72babb724c5"; }
+/******/ 		__webpack_require__.h = function() { return "7f07c9a8ad0771fe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
